d186ee1547b6963a887eecf50195b09ecdd5192e
Thang Nguyen <ngquocthang@gmail.com>
2019-06-30 07:18:20 +1000

    Add waiting page

--- README.txt
+++ README.txt
@@ -1,194 +1,67 @@
-Big Picture by HTML5 UP
+Eventually by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-This is Big Picture, a simple, single page responsive site template by HTML5 UP.
+Just a simple placeholder template for your kickass app/product/startup/whatever until it
+launches. Includes an email signup form and a cool slideshow background (more on both below).
 
-So I've been on a single page kick as of late, partly because I'm lazy, but
-mostly because they kick ass for experimentation. In this case, Big Picture
-makes heavy use of my (tentatively named and soon to be released) "scrollgress"
-and "scrollwatch" jQuery plugins to pull off some interesting effects as you
-scroll around the page. In addition to said interesting effects, Big Picture
-also includes a nice lightbox-style gallery, styling for basic page elements,
-and thoroughly commented code for your editing pleasure (with instructions!
--- see below).
+Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
+you can use for pretty much whatever.
 
-Many thanks to my good friends Felicia Simion (ineedchemicalx.deviantart.com)
-and Michael Domaradzki (md.photomerchant.net) for allowing me to use their amazing
-photos in Big Picture's demo*.
-
-(* = Not included! Only meant for use with my own on-site demo, so please do NOT
-download and/or use any of Felicia's or Michael's work without their explicit
-permission!)
+(* = not included)
 
 AJ
 aj@lkn.io | @ajlkn
 
-PS: Not sure how to get that contact form working? Give formspree.io a try (it's awesome).
+
+Signup Form:
+
+	The signup form won't actually do anything (other than report back with a "thank you" message)
+	until you tie it to either a third party service (eg. MailChimp) or your own hosted solution.
+	In either case, there are two ways to go:
+
+	1. The conventional (non-AJAX) way, which pretty much comes down to pointing the form's "action"
+	attribute to your service/script URL. If you go this route, remove the entire "Signup Form" code
+	block from assets/js/main.js (since it's not needed for this approach).
+
+	-or-
+
+	2. The AJAX way. How you set this up is largely dependent on the service/solution you're using
+	so you'll need to consult their/its documentation. However, I have included some basic code
+	(under "Signup Form" in assets/js/main.js) that will at least let you interact with the
+	form itself.
 
 
-Instructions:
+Slideshow Background:
 
-	Overview:
+	This is pretty straightforward, but there are two JS settings you'll want to be aware of
+	(found under "Slideshow Background" in assets/js/main.js):
 
-		Being a single pager, Big Picture should be way simpler to work with than
-		some of the heavier stuff I've released in the past. In fact, aside from
-		a main page <header> and <footer>, it's pretty much just a stack of "main"
-		<section> elements that follow the same basic pattern:
+	images
 
-			<section id="foobar" class="main">
-				<div class="content container">
-					<header>
-						<h2>Foobar</h2>
-					</header>
-					...
-				</div>
-			</section>
+		The list of images to cycle through, given in the following format:
 
-		The section can then be assigned a style class to determine its basic
-		look (and, in some cases, its behavior):
+			'url': 'alignment'
 
-			style1
-				Centered content with an oversized <h2>. Works best when
-				paired with a background image or color.
+		Where 'url' is the image (eg. 'images/foo.jpg', 'http://somewhere.else/foo.jpg'), and
+		'alignment' is how the image should be vertically aligned ('top', 'center', or 'bottom').
 
-			style2 left
-				Content in a box, anchored to the left side of the window. Works
-				best when paired with a background image or color. If you have
-				"useSectionTransitions" turned on in your settings, the box will
-				slide into view from the left.
+		Note: Browsers that don't support CSS transitions (like IE<=9) will only see the first image.
 
-			style2 right
-				Content in a box, anchored to the right side of the window. Works
-				best when paired with a background image or color. If you have
-				"useSectionTransitions" turned on in your settings, the box will
-				slide into view from the right.
+	delay
 
-			style3 primary
-				Used for generic content. Set against the primary background
-				color (default is white).
-
-			style3 secondary
-				Used for generic content. Set against the secondary background
-				color (default is a light gray).
-
-		Oh, and there are a few (well, two) optional modifier classes you can
-		tack on for additional effects:
-
-			dark
-				Flips the content's color scheme so it shows up better
-				against darker background images and colors.
-
-			fullscreen
-				Makes the section fill the entire window (only if "useFullScreen"
-				is enabled in your settings).
-
-
-	Lightbox Gallery:
-
- 		The actual gallery function is powered by my Poptrox plugin. For info on
- 		how that works, go here: github.com/ajlkn/jquery.poptrox
-
-		Rows:
-
-		 	The structure of the gallery is a skel grid. Each row looks like this:
-
-			 	<div class="row flush images">
-			 		<div class="6u">...<div>
-			 		<div class="6u">...<div>
-			 	</div>
-
-			The "Xu" class indicates the width of the cell (in this case, both are 6).
-			You can use any number of cells and any combination of widths provided their
-			combined widths add up to exactly 12. So, if you needed, say, three images
-			in a row, you can do something like this:
-
-			 	<div class="row flush images">
-			 		<div class="4u">...<div>
-			 		<div class="4u">...<div>
-			 		<div class="4u">...<div>
-			 	</div>
-
-			You can also use a "-Xu" class to "nudge" a cell over by that much, which
-			comes in handy if you need to place just one image on a row and you want
-			to center it:
-
-			 	<div class="row flush images">
-			 		<div class="6u -3u">...<div>
-			 	</div>
-
-			Which translates to "make this 6 wide and nudge it over to the right by
-			another 3".
-
-		Images:
-
-			Each image (the '...' bit in the above examples) should look like this:
-
-				<a href="path/to/fullsize.jpg" class="image fit from-(direction)">
-					<img src="path/to/thumbnail.jpg" title="This is the image caption." alt="" /></a>
-				</div>
-
-			The "from-(direction)" class indicates the direction from which the image should
-			slide into view, and can be any of the following:
-
-				from-left
-				from-right
-				from-bottom
-				from-left
-
-			You can also just remove the "from-(direction)" class if you don't want that particular
-			image to slide into view (in which case it'll simply fade in).
-
-
-	Contact Form:
-
-		To get this working, place a script on your server to receive the form data, then
-		point the "action" attribute to it (eg. action="http://mydomain.tld/mail.php").
-		More on how it all works here: 1stwebdesigner.com/tutorials/custom-php-contact-forms
-
-
-    Icons:
-
-     	Powered by Font Awesome. Go here for a full listing of all the icons you can use:
-     	fortawesome.github.io/Font-Awesome/cheatsheet
-
-
-	Other Stuff:
-
-		- If you don't like the way images are tinted, either change "images/overlay.png"
-		  to something else, or remove all references to it from css/style.css.
-
-		- If you plan to keep support for IE8 (what little there is), don't forget to update
-		  css/ie/ie8.css as you change other stuff.
+		How long to wait between transitions (in ms). Note that this must be at least twice as long as
+		the transition speed itself (currently 3 seconds).
 
 
 Credits:
 
 	Demo Images:
-		Felicia Simion (ineedchemicalx.deviantart.com)
-			"The Swallow Song"
-			"Mind is a clear stage"
-			"The Anonymous Red"
-			"The sparkling shell"
-			"Carry on"
-
-		Michael Domaradzki (md.photomerchant.net)
-			"Vine Country"
-			"Airchitecture II"
-			"Bent IX"
-			"Air Lounge"
+		Unsplash (unsplash.com)
 
 	Icons:
-		Font Awesome (fortawesome.github.com/Font-Awesome)
+		Font Awesome (fontawesome.io)
 
 	Other:
-		jQuery (jquery.com)
-		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
-		CSS3 PIE (css3pie.com)
-		background-size polyfill (github.com/louisremi)
-		Respond.js (j.mp/respondjs)
-		jquery.poptrox (@ajlkn)
-		jquery.scrolly (@ajlkn)
-		jquery.scrollgress (@ajlkn)
-		Skel (skel.io)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,237 +1,171 @@
 /*
-	Big Picture by HTML5 UP
+	Eventually by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function() {
 
-	skel.breakpoints({
-		xxlarge: '(max-width: 1920px)',
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 1000px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-	});
+	"use strict";
 
-	$(function() {
+	var	$body = document.querySelector('body');
 
-		var	$window = $(window),
-			$body = $('body'),
-			$header = $('#header'),
-			$all = $body.add($header);
+	// Methods/polyfills.
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
+			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 0);
-			});
+		// canUse
+			window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
 
-		// Touch mode.
-			skel.on('change', function() {
+		// window.addEventListener
+			(function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
 
-				if (skel.vars.mobile || skel.breakpoint('small').active)
-					$body.addClass('is-touch');
-				else
-					$body.removeClass('is-touch');
+	// Play initial animations on page load.
+		window.addEventListener('load', function() {
+			window.setTimeout(function() {
+				$body.classList.remove('is-preload');
+			}, 100);
+		});
 
-			});
+	// Slideshow Background.
+		(function() {
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+			// Settings.
+				var settings = {
 
-		// Fix: IE flexbox fix.
-			if (skel.vars.IEVersion <= 11
-			&&	skel.vars.IEVersion >= 10) {
+					// Images (in the format of 'url': 'alignment').
+						images: {
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
+						},
 
-				var $main = $('.main.fullscreen'),
-					IEResizeTimeout;
-
-				$window
-					.on('resize.ie-flexbox-fix', function() {
-
-						clearTimeout(IEResizeTimeout);
-
-						IEResizeTimeout = setTimeout(function() {
-
-							var wh = $window.height();
-
-							$main.each(function() {
-
-								var $this = $(this);
-
-								$this.css('height', '');
-
-								if ($this.height() <= wh)
-									$this.css('height', (wh - 50) + 'px');
-
-							});
-
-						});
-
-					})
-					.triggerHandler('resize.ie-flexbox-fix');
-
-			}
-
-		// Prioritize "important" elements on small.
-			skel.on('+small -small', function() {
-				$.prioritize(
-					'.important\\28 small\\29',
-					skel.breakpoint('small').active
-				);
-			});
-
-		// Gallery.
-			$window.on('load', function() {
-
-				var $gallery = $('.gallery');
-
-				$gallery.poptrox({
-					baseZIndex: 10001,
-					useBodyOverflow: false,
-					usePopupEasyClose: false,
-					overlayColor: '#1f2328',
-					overlayOpacity: 0.65,
-					usePopupDefaultStyling: false,
-					usePopupCaption: true,
-					popupLoaderText: '',
-					windowMargin: 50,
-					usePopupNav: true
-				});
-
-				// Hack: Adjust margins when 'small' activates.
-					skel
-						.on('-small', function() {
-							$gallery.each(function() {
-								$(this)[0]._poptrox.windowMargin = 50;
-							});
-						})
-						.on('+small', function() {
-							$gallery.each(function() {
-								$(this)[0]._poptrox.windowMargin = 5;
-							});
-						});
-
-			});
-
-		// Section transitions.
-			if (skel.canUse('transition')) {
-
-				var on = function() {
-
-					// Galleries.
-						$('.gallery')
-							.scrollex({
-								top:		'30vh',
-								bottom:		'30vh',
-								delay:		50,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
-
-					// Generic sections.
-						$('.main.style1')
-							.scrollex({
-								mode:		'middle',
-								delay:		100,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
-
-						$('.main.style2')
-							.scrollex({
-								mode:		'middle',
-								delay:		100,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
-
-					// Contact.
-						$('#contact')
-							.scrollex({
-								top:		'50%',
-								delay:		50,
-								initialize:	function() { $(this).addClass('inactive'); },
-								terminate:	function() { $(this).removeClass('inactive'); },
-								enter:		function() { $(this).removeClass('inactive'); },
-								leave:		function() { $(this).addClass('inactive'); }
-							});
+					// Delay.
+						delay: 6000
 
 				};
 
-				var off = function() {
+			// Vars.
+				var	pos = 0, lastPos = 0,
+					$wrapper, $bgs = [], $bg,
+					k, v;
 
-					// Galleries.
-						$('.gallery')
-							.unscrollex();
+			// Create BG wrapper, BGs.
+				$wrapper = document.createElement('div');
+					$wrapper.id = 'bg';
+					$body.appendChild($wrapper);
 
-					// Generic sections.
-						$('.main.style1')
-							.unscrollex();
+				for (k in settings.images) {
 
-						$('.main.style2')
-							.unscrollex();
+					// Create BG.
+						$bg = document.createElement('div');
+							$bg.style.backgroundImage = 'url("' + k + '")';
+							$bg.style.backgroundPosition = settings.images[k];
+							$wrapper.appendChild($bg);
 
-					// Contact.
-						$('#contact')
-							.unscrollex();
+					// Add it to array.
+						$bgs.push($bg);
+
+				}
+
+			// Main loop.
+				$bgs[pos].classList.add('visible');
+				$bgs[pos].classList.add('top');
+
+				// Bail if we only have a single BG or the client doesn't support transitions.
+					if ($bgs.length == 1
+					||	!canUse('transition'))
+						return;
+
+				window.setInterval(function() {
+
+					lastPos = pos;
+					pos++;
+
+					// Wrap to beginning if necessary.
+						if (pos >= $bgs.length)
+							pos = 0;
+
+					// Swap top images.
+						$bgs[lastPos].classList.remove('top');
+						$bgs[pos].classList.add('visible');
+						$bgs[pos].classList.add('top');
+
+					// Hide last image after a short delay.
+						window.setTimeout(function() {
+							$bgs[lastPos].classList.remove('visible');
+						}, settings.delay / 2);
+
+				}, settings.delay);
+
+		})();
+
+	// Signup Form.
+		(function() {
+
+			// Vars.
+				var $form = document.querySelectorAll('#signup-form')[0],
+					$submit = document.querySelectorAll('#signup-form input[type="submit"]')[0],
+					$message;
+
+			// Bail if addEventListener isn't supported.
+				if (!('addEventListener' in $form))
+					return;
+
+			// Message.
+				$message = document.createElement('span');
+					$message.classList.add('message');
+					$form.appendChild($message);
+
+				$message._show = function(type, text) {
+
+					$message.innerHTML = text;
+					$message.classList.add(type);
+					$message.classList.add('visible');
+
+					window.setTimeout(function() {
+						$message._hide();
+					}, 3000);
 
 				};
 
-				skel.on('change', function() {
+				$message._hide = function() {
+					$message.classList.remove('visible');
+				};
 
-					if (skel.breakpoint('small').active)
-						(off)();
-					else
-						(on)();
+			// Events.
+			// Note: If you're *not* using AJAX, get rid of this event listener.
+				$form.addEventListener('submit', function(event) {
+
+					event.stopPropagation();
+					event.preventDefault();
+
+					// Hide message.
+						$message._hide();
+
+					// Disable submit.
+						$submit.disabled = true;
+
+					// Process form.
+					// Note: Doesn't actually do anything yet (other than report back with a "thank you"),
+					// but there's enough here to piece together a working AJAX submission call that does.
+						window.setTimeout(function() {
+
+							// Reset form.
+								$form.reset();
+
+							// Enable submit.
+								$submit.disabled = false;
+
+							// Show message.
+								$message._show('success', 'Thank you!');
+								//$message._show('failure', 'Something went wrong. Please try again.');
+
+						}, 750);
 
 				});
 
-			}
+		})();
 
-		// Events.
-			var resizeTimeout, resizeScrollTimeout;
-
-			$window
-				.resize(function() {
-
-					// Disable animations/transitions.
-						$body.addClass('is-resizing');
-
-					window.clearTimeout(resizeTimeout);
-
-					resizeTimeout = window.setTimeout(function() {
-
-						// Update scrolly links.
-							$('a[href^="#"]').scrolly({
-								speed: 1500,
-								offset: $header.outerHeight() - 1
-							});
-
-						// Re-enable animations/transitions.
-							window.setTimeout(function() {
-								$body.removeClass('is-resizing');
-								$window.trigger('scroll');
-							}, 0);
-
-					}, 100);
-
-				})
-				.load(function() {
-					$window.trigger('resize');
-				});
-
-	});
-
-})(jQuery);
+})();
